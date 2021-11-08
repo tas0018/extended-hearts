50b863feee21678810080afdb3e93ea627ee2bb7
Taylor Smith <tas0018@auburn.edu>
2021-11-08 15:24:57 -0600

    started navbar

---
+++ eternal-hearts-v1/src/components/Header.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
+
+const Header = () => {
+  return (
+    <Nav>
+      <NavLink to="/">EH</NavLink>
+    </Nav>
+  )
+}
+
+export default Header
